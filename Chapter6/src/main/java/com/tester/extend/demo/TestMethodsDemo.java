ae4ef4b002dba83abc17379d88a278f3a8a7f854
linjb <linjinbo@sysware.com>
2018-04-19 15:34:19 +0800

    配置extendreport

---
+++ Chapter6/src/main/java/com/tester/extend/demo/TestMethodsDemo.java
@@ -0,0 +1,33 @@
+package com.tester.extend.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMethodsDemo {
+
+    /**
+     * 测试模板
+     * Extentreports
+     */
+    @Test
+    public void test1() {
+        Assert.assertEquals(1, 2);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertEquals("aaa", "aaa");
+    }
+
+    @Test
+    public void logDemo() {
+        Reporter.log("这是测试用的日志");
+        throw new RuntimeException("这是测试时抛出的运行时异常");
+    }
+}
